b93bafe573736f8ed36186669ef5e235977aacae
allen <coding.example02.js@gmail.com>
2020-04-10 21:42:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+// express application
+const app = express();
+
+// Dynamically changed PORT number
+const PORT = process.env.PORT || 5000;
+
+console.log(`Serve is running at port ${PORT}`);
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+app.listen(PORT);
