273821f4f8078e0761759974ec3c546e724ea578
allen <allen.simbul591@gmail.com>
2020-05-05 13:28:58 +0100

    code clean up

--- index.js
+++ index.js
@@ -1,44 +1,15 @@
-import express from 'express';
-import passport from 'passport';
-import GoogleStrategy from 'passport-google-oauth20';
-import dotenv from 'dotenv';
+const express = require('express');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
-dotenv.config();
+console.log(process.env);
 
 // express application
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
-    }
-  )
-);
+authRoutes(app);
 
 // Dynamically changed PORT number
 const PORT = process.env.PORT || 5000;
-
 console.log(`Serve is running at port ${PORT}`);
-
-app.get('/', (req, res) => {
-  res.send({ hi: 'Allen' });
-});
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const dotenv = require('dotenv');
+dotenv.config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      // console.log(refreshToken);
+      // console.log(profile);
+    }
+  )
+);
