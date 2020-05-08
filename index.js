a54833d4261c21cef95373c0ce5cf6ca3d58f66b
allen <allen.simbul591@gmail.com>
2020-05-08 18:03:41 +0100

    added user routes

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 require('./db/mongoose');
 require('./services/passport');
+
+// routes
 const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routers/User-Router');
 
 // express application
 const app = express();
 
 app.use(express.json());
 app.use(authRoutes);
+app.use(userRoutes);
 
 // Dynamically changed PORT number
 const PORT = process.env.PORT || 5000;
---
+++ routers/User-Router.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = new express.Router();
+const User = require('../models/User');
+
+module.exports = router;
