2ce10bed51fa87efefa8c90825273259b53b5afd
allen <allen.simbul591@gmail.com>
2020-05-12 14:04:07 +0100

    made route for logging out users

--- routes/GoogleAuth-Routes.js
+++ routes/GoogleAuth-Routes.js
@@ -51,4 +51,9 @@ router.get('/api/current_user', (req, res) => {
   res.send(req.user);
 });
 
+router.get('/api/logout', (req, res) => {
+  req.logout();
+  res.send('You are logged out');
+});
+
 module.exports = router;
