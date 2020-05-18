e13f19be51933c7cdfacb98f8afd4adbea396db9
allen <allen.simbul591@gmail.com>
2020-05-18 19:50:33 +0100

    rewrote middlware files

---
+++ middlewares/requireCredits.js
@@ -0,0 +1,13 @@
+const creditCheck = async (req, res, next) => {
+  console.log('credit check middlware is running');
+  try {
+    if (req.user.credits < 1) {
+      throw new Error();
+    }
+    next();
+  } catch (e) {
+    return res.status(401).send({ error: 'Not Enough credits!' });
+  }
+};
+
+module.exports = creditCheck;
--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,8 +1,13 @@
-module.exports = (req, res, next) => {
-  console.log('hello');
-  if (!req.user) {
+const auth = async (req, res, next) => {
+  console.log('auth middleware is running');
+  try {
+    if (!req.user) {
+      throw new Error();
+    }
+    next();
+  } catch (e) {
     return res.status(401).send({ error: 'You must log in!' });
   }
-
-  next();
 };
+
+module.exports = auth;
